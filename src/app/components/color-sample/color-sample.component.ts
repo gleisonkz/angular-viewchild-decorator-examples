40b3d1b3a9d0c5901f4956c18c62eb6ef2e488d6
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-01-26 16:55:17 -0300

    ✨ feat: color-sample component.

---
+++ src/app/components/color-sample/color-sample.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'color-sample',
+  templateUrl: './color-sample.component.html',
+  styleUrls: ['./color-sample.component.scss'],
+})
+export class ColorSampleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
