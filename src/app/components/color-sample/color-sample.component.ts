2c62a2e4f59e518af9934ba61452512fbf6a9d80
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-01-26 21:24:47 -0300

    ✨ feat: color-sample-service creation.

--- src/app/components/color-sample/color-sample.component.ts
+++ src/app/components/color-sample/color-sample.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { ColorSampleService } from 'src/app/services/color-sample.service';
 
 @Component({
   selector: 'color-sample',
   templateUrl: './color-sample.component.html',
   styleUrls: ['./color-sample.component.scss'],
+  providers: [{ provide: ColorSampleService }],
 })
 export class ColorSampleComponent {
   constructor() {}
---
+++ src/app/services/color-sample.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ColorSampleService {
+  constructor() {}
+
+  log() {
+    console.log('ColorSampleService');
+  }
+}
