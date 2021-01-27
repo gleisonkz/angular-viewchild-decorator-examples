b569ed8619420780e600fbfe68be5ddc0008870f
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-01-27 07:16:23 -0300

    ✨ feat: mat-input example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 import { ColorPickerDirective } from 'ngx-color-picker';
 import { ColorSampleComponent } from './components/color-sample/color-sample.component';
 import { ColorSampleService } from './services/color-sample.service';
@@ -13,17 +14,21 @@ export class AppComponent implements AfterViewInit {
   title = 'angular-viewchild-decorator-examples';
   primary = '#1976d2';
 
+  ngAfterViewInit(): void {
+    this.service.log();
+    console.log('matInput', this.matInput);
+  }
+
   // recuperando a referencia de um serviço
   @ViewChild(ColorSampleService)
   service: ColorSampleService;
 
-  ngAfterViewInit(): void {
-    this.service.log();
-  }
-
   // recuperando a referencia de diretivas
   @ViewChild('primaryInput', { read: ColorPickerDirective })
   colorPicker: ColorPickerDirective;
+
+  @ViewChild('primaryInput', { read: MatInput })
+  matInput: MatInput;
 
   // recuperando a referencia do elemento DOM de um componente
   @ViewChild('primaryColorSample', { read: ElementRef })
