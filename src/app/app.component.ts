8f2415c908f2a87917eb822eefa47201b639926a
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-01-26 21:16:51 -0300

    📦 refactor: organizing examples.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { ColorPickerDirective } from 'ngx-color-picker';
+import { ColorSampleComponent } from './components/color-sample/color-sample.component';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-viewchild-decorator-examples';
   primary = '#1976d2';
+
+  // recuperando a referencia de diretivas
+  @ViewChild('primaryInput', { read: ColorPickerDirective })
+  colorPicker: ColorPickerDirective;
+
+  // recuperando a referencia do elemento DOM de um componente
+  @ViewChild('primaryColorSample', { read: ElementRef })
+  sample: ElementRef;
+
+  // recuperando a referencia de um elemento DOM via template variable
+  @ViewChild('title')
+  titleElement: ElementRef;
+
+  // recuperando a referencia de um componente
+  @ViewChild(ColorSampleComponent)
+  colorSampleComponent: ColorSampleComponent;
+
+  // tentando recuperar a referencia de um componente dentro de outro
+  @ViewChild(MatIcon)
+  matIcon: MatIcon;
+
+  openColorPicker() {
+    this.colorPicker.openDialog();
+  }
 }
